d900d3988f06f5fc5f19626465de695d38504c8d
Fabian Meyer <3982806+meyfa@users.noreply.github.com>
2022-03-26 14:49:22 +0100

    feat: Implement onTermination() for process exit signal handling (#12)
    * feat: Implement onTermination() for process exit signal handling
    
    The new function onTermination() can be used to register callbacks for
    when the process receives a SIGTERM or SIGINT signal. These callbacks
    will be run in reverse (compared to the order in which they were added).
    After processing all callbacks, the process will exit explicitly.
    Callbacks can be async by returning a Promise.
    
    Tests are added for this behavior on POSIX systems. These same tests
    cannot be replicated on Windows due to lack of signal support. Still,
    Node.js will emulate SIGINT when CTRL+C is pressed in a Windows shell,
    meaning the effectiveness of this code on Windows is still pretty much
    guaranteed, we simply cannot run the tests.
    
    * tests: Fix line terminator handling in termination.test.ts

---
+++ lib/termination.ts
@@ -0,0 +1,48 @@
+import { constants } from 'os'
+
+export type TerminationCallback = (signal: NodeJS.Signals) => (void | Promise<void>)
+
+class Terminator {
+  private exiting = false
+  private readonly callbacks: TerminationCallback[] = []
+
+  async handleSignal (signal: NodeJS.Signals): Promise<void> {
+    if (this.exiting) {
+      return
+    }
+    this.exiting = true
+    // Invoke in reverse order in case any callbacks registered later depend on things registered earlier
+    for (let i = this.callbacks.length - 1; i >= 0; --i) {
+      await this.callbacks[i](signal)
+    }
+    // POSIX process exit code is typically 128+signal, such as 143 for SIGTERM
+    process.exit(128 + constants.signals[signal])
+  }
+
+  registerCallback (callback: TerminationCallback): void {
+    this.callbacks.push(callback)
+  }
+}
+
+const handler = new Terminator()
+
+for (const signal of ['SIGTERM', 'SIGINT'] as const) {
+  process.once(signal, () => {
+    void handler.handleSignal(signal)
+  })
+}
+
+/**
+ * Register a callback for when termination of the process is requested via an external signal. When a signal is
+ * received, all callbacks will be run serially in reverse order, and then the process will exit.
+ * Explicit termination such as via process.exit() will not invoke any of the callbacks.
+ *
+ * The callback can be asynchronous, returning a Promise. Still it is required to limit the total amount of time spent
+ * in termination callbacks, since the OS (or container orchestration) might forcefully kill the process a few seconds
+ * after the signal.
+ *
+ * @param fn The callback to register.
+ */
+export function onTermination (fn: TerminationCallback): void {
+  handler.registerCallback(fn)
+}
---
+++ test/termination.test.ts
@@ -0,0 +1,114 @@
+import chai, { expect } from 'chai'
+import chaiAsPromised from 'chai-as-promised'
+import { ChildProcess, fork } from 'child_process'
+import { setTimeout as delay } from 'timers/promises'
+import * as url from 'url'
+
+chai.use(chaiAsPromised)
+
+async function waitUntil (condition: () => boolean, timeout: number): Promise<void> {
+  const start = Date.now()
+  while (!condition()) {
+    if (Date.now() - start > timeout) {
+      throw new Error('timeout')
+    }
+    await delay(100)
+  }
+}
+
+describe('/lib/termination', function () {
+  const FIXTURE_PATH = url.fileURLToPath(new URL('termination-fixture.ts', import.meta.url))
+
+  async function performSignalTest (signal: NodeJS.Signals): Promise<{ fixture: ChildProcess, outLines: string[] }> {
+    const fixture = fork(FIXTURE_PATH, {
+      // Make available stdout and stderr
+      silent: true,
+      // Run with ts-node loader
+      execArgv: ['--loader=ts-node/esm']
+    })
+
+    expect(fixture.stdout).to.be.an('object')
+    expect(fixture.stderr).to.be.an('object')
+
+    const outLines: string[] = []
+    const errLines: string[] = []
+    fixture.stdout?.on('data', (chunk: Buffer) => {
+      const lines = chunk.toString().split('\n').filter(line => line.length > 0)
+      outLines.push(...lines)
+    })
+    fixture.stderr?.on('data', (chunk: Buffer) => {
+      // Filter out warnings emitted due to presence of ts-node loader
+      const lines = chunk.toString().split('\n').filter(line => line.length > 0 && !/--experimental-loader|--trace-warnings/.test(line))
+      errLines.push(...lines)
+    })
+
+    await waitUntil(() => (outLines.length + errLines.length) > 0, 6000)
+    expect(fixture.kill(signal)).to.equal(true)
+    await waitUntil(() => fixture.exitCode != null || fixture.signalCode != null, 4000)
+
+    expect(errLines).to.deep.equal([])
+
+    return { fixture, outLines }
+  }
+
+  it('runs callbacks on SIGTERM', async function () {
+    // Windows has no signal support
+    if (process.platform === 'win32') {
+      this.skip()
+      return
+    }
+
+    this.slow(3000)
+    this.timeout(10000)
+
+    const { fixture, outLines } = await performSignalTest('SIGTERM')
+
+    expect(fixture.exitCode).to.equal(143)
+    expect(outLines).to.deep.equal([
+      'started',
+      't2: SIGTERM',
+      't1: SIGTERM',
+      't0: SIGTERM'
+    ])
+  })
+
+  it('runs callbacks on SIGINT', async function () {
+    // Windows has no signal support
+    if (process.platform === 'win32') {
+      this.skip()
+      return
+    }
+
+    this.slow(3000)
+    this.timeout(10000)
+
+    const { fixture, outLines } = await performSignalTest('SIGINT')
+
+    expect(fixture.exitCode).to.equal(130)
+    expect(outLines).to.deep.equal([
+      'started',
+      't2: SIGINT',
+      't1: SIGINT',
+      't0: SIGINT'
+    ])
+  })
+
+  it('does not run callbacks on SIGKILL', async function () {
+    // Windows has no signal support
+    if (process.platform === 'win32') {
+      this.skip()
+      return
+    }
+
+    this.slow(3000)
+    this.timeout(10000)
+
+    const { fixture, outLines } = await performSignalTest('SIGKILL')
+
+    expect(fixture.exitCode).to.equal(null)
+    expect(fixture.signalCode).to.equal('SIGKILL')
+    expect(outLines).to.deep.equal([
+      'started'
+    ])
+  })
+})
