52e36d7f3cc86ca68eb064b7c2e4d871a95f5d16
Fabian Meyer <3982806+meyfa@users.noreply.github.com>
2023-02-15 23:23:25 +0100

    chore: Replace chai with native 'node:assert' (#87)
    Fewer dependencies!

--- test/server-promises.test.ts
+++ test/server-promises.test.ts
@@ -1,10 +1,7 @@
-import chai, { expect } from 'chai'
-import chaiAsPromised from 'chai-as-promised'
+import assert from 'node:assert'
 import http from 'http'
 import express from 'express'
 import { promisifiedClose, promisifiedListen } from '../src/server-promises.js'
-
-chai.use(chaiAsPromised)
 
 describe('server-promises.ts', function () {
   const serversToCleanUp: http.Server[] = []
@@ -20,21 +17,25 @@ describe('server-promises.ts', function () {
     it('resolves with a started server', async function () {
       const server = await promisifiedListen(express(), 3333)
       serversToCleanUp.push(server)
-      expect(server.listening).to.equal(true)
-      expect(server.address()).to.have.property('port').that.equals(3333)
+      assert.strictEqual(server.listening, true)
+      const address = server.address()
+      assert.ok(typeof address === 'object')
+      assert.strictEqual(address?.port, 3333)
     })
 
     it('rejects if trying to start on a used port', async function () {
       serversToCleanUp.push(await promisifiedListen(express(), 3333))
-      await expect(promisifiedListen(express(), 3333)).to.eventually.be.rejectedWith('EADDRINUSE')
+      await assert.rejects(promisifiedListen(express(), 3333), { code: 'EADDRINUSE' })
     })
 
     it('works for plain http.Server', async function () {
       // The function was initially implemented for express.Application only, but expanded to be more generic.
       const server = await promisifiedListen(http.createServer(), 3333)
       serversToCleanUp.push(server)
-      expect(server.listening).to.equal(true)
-      expect(server.address()).to.have.property('port').that.equals(3333)
+      assert.strictEqual(server.listening, true)
+      const address = server.address()
+      assert.ok(typeof address === 'object')
+      assert.strictEqual(address?.port, 3333)
     })
   })
 
@@ -42,23 +43,24 @@ describe('server-promises.ts', function () {
     it('resolves as soon as the server is closed', async function () {
       const server = http.createServer().listen(3333)
       serversToCleanUp.push(server)
-      expect(server.address()).to.have.property('port').that.equals(3333)
-      await expect(promisifiedClose(server)).to.eventually.be.fulfilled
-      expect(server.listening).to.equal(false)
-      expect(server.address()).to.equal(null)
+      assert.strictEqual(server.listening, true)
+      assert.ok(server.address())
+      await promisifiedClose(server)
+      assert.strictEqual(server.listening, false)
+      assert.strictEqual(server.address(), null)
     })
 
     it('rejects if the server was not running', async function () {
       const server = http.createServer()
-      expect(server.address()).to.equal(null)
-      await expect(promisifiedClose(server)).to.eventually.be.rejectedWith('not running')
+      assert.strictEqual(server.address(), null)
+      await assert.rejects(promisifiedClose(server), /not running/)
     })
 
     it('rejects if called twice on the same instance', async function () {
       const server = http.createServer().listen(3333)
       serversToCleanUp.push(server)
-      await expect(promisifiedClose(server)).to.eventually.be.fulfilled
-      await expect(promisifiedClose(server)).to.eventually.be.rejectedWith('not running')
+      await assert.doesNotReject(promisifiedClose(server))
+      await assert.rejects(promisifiedClose(server), /not running/)
     })
   })
 })
